fb37852b7f0cbd13560828f45fc15718a97c63fb
ivanlele <ivanlele402@gmail.com>
2025-02-15 17:08:20 +0200

    Update minimum iOS platform version to 13 and upgrade TensorFlowLiteC binary targets to version 1.0.3

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "TensorFlowLiteC",
-    platforms: [.iOS(.v16)],
+    platforms: [.iOS(.v13)],
     products: [
         .library(
             name: "TensorFlowLiteC",
@@ -17,9 +17,9 @@ let package = Package(
         ),
     ],
     targets: [
-        .binaryTarget(name: "TensorFlowLiteC", url: "https://github.com/rarimo/TensorFlowLiteC/releases/download/v1.0.2/TensorFlowLiteC.xcframework.zip", checksum: "9346c784977b97b106df49640b9d128a039246c32f8053caf22fe9c79905def3"),
-        .binaryTarget(name: "TensorFlowLiteCCoreML", url: "https://github.com/rarimo/TensorFlowLiteC/releases/download/v1.0.2/TensorFlowLiteCCoreML.xcframework.zip", checksum: "5aa807205f50c2eeb60df9633a09003f833c3250d225bc1658063d97beac10fd"),
-        .binaryTarget(name: "TensorFlowLiteCMetal", url: "https://github.com/rarimo/TensorFlowLiteC/releases/download/v1.0.2/TensorFlowLiteCMetal.xcframework.zip", checksum: "0d977f33b35fefc37336e2e5b72abdd56c2c4727a227944e058278de0dd96539"),
+        .binaryTarget(name: "TensorFlowLiteC", url: "https://github.com/rarimo/TensorFlowLiteC/releases/download/v1.0.3/TensorFlowLiteC.xcframework.zip", checksum: "a838c7354498cb628529b70f76d2e7b8d5559776fced14cd96ff0baa25099093"),
+        .binaryTarget(name: "TensorFlowLiteCCoreML", url: "https://github.com/rarimo/TensorFlowLiteC/releases/download/v1.0.3/TensorFlowLiteCCoreML.xcframework.zip", checksum: "82f8a91146a2f08c0859c770b3c74b54e2ef0a3b473e634a35180b64f6cfee71"),
+        .binaryTarget(name: "TensorFlowLiteCMetal", url: "https://github.com/rarimo/TensorFlowLiteC/releases/download/v1.0.3/TensorFlowLiteCMetal.xcframework.zip", checksum: "9c53adcd08ba9ebcd3b007d3fa2f28f1da5267f5858bd006a1e48aff57bdb57c"),
         .target(name: "Link",
                 linkerSettings: [.linkedLibrary("c++")]),
     ]
